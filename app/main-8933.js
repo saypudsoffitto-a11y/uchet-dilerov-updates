dd4ddab1094ba3011c805fbc7bab24d5c519fba0
saypudsoffitto-a11y <saypudsoffitto@gmail.com>
2026-09-11 01:38:40 +0300

    Add consolidated 8.9.33 main entry

---
+++ app/main-8933.js
@@ -0,0 +1,6 @@
+require('./audio-service.js');
+require('./speech-fallback-8933.js');
+require('./release-8930-main.js');
+require('./release-8933-main.js');
+require('./main-8922.js');
+require('./updater-8931.js');
